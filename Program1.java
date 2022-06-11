50aa3c5e8aab6f6d372c53faf4a359c7dfa43b63
MONIKA1234321 <106865915+MONIKA1234321@users.noreply.github.com>
2022-06-11 10:46:13 +0530

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -1,5 +1,6 @@
 class Program1{
   public static void main(Strign [] args){
-    System.out.print("THis is Program1");
+    System.out.println("THis is Program1");
+    System.out.println("Edited Version");
   }
 }
